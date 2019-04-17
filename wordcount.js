c51374217c57f5a4533a32ce2bf34edbb3d91d04
kshitiz <kshit96@gmail.com>
2019-04-17 17:13:58 +0530

    Implement code to ignore leading/trailing spaces

--- wordcount.js
+++ wordcount.js
@@ -1,17 +1,14 @@
-export const wordCount = (sentence) =>{
+export const wordCount = (sentence) => {
 
-
-  let words=sentence.split(" ");
-  var wordCountMap= new Map ();
-
-
-  for(var word of words){
-    if(wordCountMap.get(word)) {
-      wordCountMap.set(word,wordCountMap.get(word) + 1);
-    }
-    else {
-      wordCountMap.set(word,1);
+  let trimmedSentence = sentence.trim();
+  let words = trimmedSentence.split(" ");
+  var wordCountMap = new Map();
+  for (var word of words) {
+    if (wordCountMap.get(word)) {
+      wordCountMap.set(word, wordCountMap.get(word) + 1);
+    } else {
+      wordCountMap.set(word, 1);
     }
   }
   return wordCountMap;
-}
+};
